dbc4dd240555f1834c106d566e9e126f8e8a892f
infected-by-js <infectedbyjs@gmail.com>
2022-06-01 15:14:31 +1000

    add text to map

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ const margin = {
   left: 50,
 };
 
-const width = 900 - margin.left - margin.right;
-const height = 900 - margin.top - margin.bottom;
+const width = 1100 - margin.left - margin.right;
+const height = 1100 - margin.top - margin.bottom;
 
 const drawTree = (dataset) => {
   const hierarchy = d3
@@ -23,21 +23,46 @@ const drawTree = (dataset) => {
   const treemap = d3.treemap().size([width, height]).padding(2);
   const root = treemap(hierarchy);
 
-  d3.select("#chart")
+  const chartSvg = d3
+    .select("#chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .attr("transform", `translate(${margin.left}, ${margin.top})`)
-    .append("g")
-    .selectAll("rect")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // prettier-ignore
+  const cell = chartSvg
+    .selectAll("g")
     .data(root.leaves())
     .enter()
+    .append("g")
+    .attr("transform", d => `translate(${d.x0},${d.y0})`)
+
+  cell
     .append("rect")
-    .attr("x", ({ x0 }) => x0)
-    .attr("y", ({ y0 }) => y0)
     .attr("width", ({ x0, x1 }) => x1 - x0)
     .attr("height", ({ y0, y1 }) => y1 - y0)
     .attr("fill", ({ data }) => d3.interpolatePRGn(data.maturity));
+
+  cell
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("width", ({ x1, x0 }) => x1 - x0)
+    .attr("height", ({ y1, y0 }) => y1 - y0)
+    .attr("font-size", ({ x1, x0, y0, y1 }) => Math.min(x1 - x0, y1 - y0) / 6)
+    .selectAll("tspan")
+    .data(({ data }) => data.name.split(/(?=[A-Z][^A-Z])/g))
+    .enter()
+    .append("tspan")
+    .style("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .attr("dy", "2em")
+    .attr("x", function () {
+      const parentData = d3.select(this.parentNode).datum();
+      return (parentData.x1 - parentData.x0) / 2;
+    })
+
+    .text((d) => d);
 };
 const formattedData = formatData("chart title", data);
 drawTree(formattedData);
